06e7176e763ce2771ac4536825a3673254d678c0
scottysseus <sweidenkopf@gmail.com>
2022-12-17 17:43:43 -0500

    scaffolded game FSM

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,7 +1,30 @@
-export interface GameState {
-    // mapping between teams and points
-    score: Map<string, number>
+import { startTurn, TurnState } from "./turn";
 
-    // the team whose turn it is
-    teamInTurn: string
+export interface GameState {
+  // mapping between teams and points
+  score: Map<string, number>;
+
+  // the team whose turn it is
+  teamInTurn: string;
+
+  turnState: TurnState;
 }
+
+/**
+ * finishTurn updates the game state with the results of the current turn.
+ * It also updates the game state's turn state to prepare for the next turn.
+ * TODO calculate the score from the target and guess
+ * TODO update the score team
+ * TODO determine if the game is finished here
+ * @param state current state to update
+ */
+export function finishTurn(state: GameState): GameState {
+  return { ...state, turnState: startTurn(state.turnState) };
+}
+
+/**
+ * updateTurn performs all other turn state updates on the given game state.
+ * @param state GameState to update
+ * @param turnState the new TurnState for this game
+ */
+export function updateTurn(state: GameState, turnState: TurnState);
--- src/game/turn.ts
+++ src/game/turn.ts
@@ -1,48 +1,52 @@
 export enum Actors {
-    Psychic = 'psychic',
-    Player = 'player'
+  Psychic = "psychic",
+  Player = "player",
 }
 
 export interface Spectrum {
-    ends: [string, string]
+  ends: [string, string];
 }
 
 export interface TurnState {
-    actor: Actors.Player | Actors.Psychic
-    spectrum: Spectrum
-    target: number
-    clue: string
-    guess: number
+  actor: Actors.Player | Actors.Psychic;
+  spectrum: Spectrum;
+  target: number;
+  clue: string;
+  guess: number;
 }
 
 /**
  * TODO
  */
 function randomSpectrum(): Spectrum {
-    return {ends: ['likeable', 'obnoxious']}
+  return { ends: ["likeable", "obnoxious"] };
 }
 
 /**
  * TODO
  */
 function randomTarget(): number {
-    return 90
+  return 90;
 }
 
 /**
- * 
+ *
  * @returns AwaitingClue TurnState
  */
-export function startTurn(): TurnState {
-    return {
-        actor: Actors.Psychic,
-        spectrum: randomSpectrum(),
-        target: randomTarget(),
-        clue: '',
-        guess: 0
-    }
+export function startTurn(state: TurnState): TurnState {
+  return {
+    actor: Actors.Psychic,
+    spectrum: randomSpectrum(),
+    target: randomTarget(),
+    clue: "",
+    guess: 0,
+  };
 }
 
 export function submitClue(state: TurnState, clue: string) {
-    return {...state, actor: Actors.Player, clue}
+  return { ...state, actor: Actors.Player, clue };
+}
+
+export function submitGuess(state: TurnState, guess: string) {
+  return { ...state, guess };
 }
